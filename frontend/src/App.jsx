2c0f5fea69670fd05af1aba955834f6ab5b988e9
Luis <jorgex4p@gmail.com>
2025-04-30 18:59:33 -0500

    v5

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a IA-V8</h1>
+        <p>Proyecto básico de React desplegado en GitHub Pages.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
